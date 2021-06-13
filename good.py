2f7f8c42ff6b98db49e8465f58abe73906179e60
Matin mirzaei <82929860+mtiinn@users.noreply.github.com>
2021-06-13 12:57:53 +0430

    Create good.py

---
+++ good.py
@@ -0,0 +1,9 @@
+class Good:
+     scores = []
+     def __init__(self,name,price,number,Id,deposit_date,seller):
+        self.seller = seller
+        self.__name=naame
+        self.__price=price
+        self.__number=number
+        self.__Id=Id
+        self.deposit_date = deposit_date
