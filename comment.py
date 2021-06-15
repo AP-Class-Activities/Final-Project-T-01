f74a316c107b1c8e2d69dd3af0151b87197e85f5
amirhossein <79211332+capamir@users.noreply.github.com>
2021-06-15 20:59:34 +0430

    Update comment.py

--- comment.py
+++ comment.py
@@ -1,12 +1,19 @@
 class  Comment:    
-    def __init__(self,good,customer,text,Id):
+    def __init__(self,good,customer,text):
+        #self.__Id = ID
         self.__good = good
         self.__text = text
         self.__customer = customer 
-        self.__Id = Id
+
+    #getter
+    @property    
     def get_good(self):
-         return self.__good
+        return self.__good
+
+    @property
     def get_customer(self):
         return self.__customer
+
+    @property
     def get_text(self):\
         return self.__text
