8db34b535ec59e3b152c059329ba089b1a2707c4
amirhossein <79211332+capamir@users.noreply.github.com>
2021-06-15 20:26:39 +0430

    Update good.py

--- good.py
+++ good.py
@@ -1,9 +1,22 @@
 class Good:
      scores = []
-     def __init__(self,name,price,number,Id,deposit_date,seller):
+     def __init__(self,id,name,price,number,deposit_date,seller):
         self.seller = seller
-        self.__name=naame
+        self.__id=id
+        self.__name=name
         self.__price=price
         self.__number=number
-        self.__Id=Id
+        
         self.deposit_date = deposit_date
+      
+      #setters & getters
+      @property
+      def id(self):
+         return self.__id
+
+      @id.setter
+      def id(self,value):
+         self.__id = value 
+
+
+      
